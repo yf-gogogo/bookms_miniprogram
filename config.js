1ae09ed528a12cf3c0222ca25a11e48c7992634e
马燕峰 <l>
2018-07-01 16:52:35 +0800

    7.1提交

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
   // url:'http://122.204.161.247/',
   // url:'http://localhost:3000/',
   // url: 'http://192.168.1.100:3000/',
-  url:'http://111.230.89.209/',
+  // url:'http://111.230.89.209/',
+  // url:'https://james.letec.top/1140357028/'
+  url:'https://wxnercel.ccnu.edu.cn/bookms/'
 }
